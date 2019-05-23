0db56a971429301dc6f14c184e1398c5df370fba
Aji R N <dev@ajithrn.com>
2019-05-23 14:15:34 +0530

    - code cleanup and improvements. - get rid of unwanted assets

--- admin/js/primary-category-selector-admin.js
+++ admin/js/primary-category-selector-admin.js
@@ -1,43 +1,14 @@
 (function($) {
 	'use strict';
 
-	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-
 	var taxonomies = primaryCategorySelector.taxonomies;
 
 	/**
-	 * Function to dynamically change the vaule of primary taxonomy/cat selector
+	 * Function to dynamically change the value of primary taxonomy/cat selector
 	 * @param {*} taxonomyName
 	 */
 	function termCheckboxHandler ( taxonomyName, isGutenberg ) {
 		return function() {
-			console.log(taxonomyName);
 			if (true === isGutenberg ) {
 				var label = $("label[for='" + this.id + "']").text();
 			} else {
